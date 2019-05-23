4a261bc4741c99dc3f592517a120d5c6c6d0b12a
KaranDahiya <41240707+KaranDahiya@users.noreply.github.com>
2019-05-23 13:55:40 -0400

    adding code files

---
+++ menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    // loads scene with game instructions
+    public void loadControlsScene()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    // starts game
+    public void loadGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    // returns to menu
+    public void loadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // exits application
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ objectiveController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectiveController : MonoBehaviour
+{
+    // combat variables
+    public playerController playerController;
+    public RectTransform objectiveHealthBar;
+    public float objectiveHealth;
+    public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // displays current player health bar
+        if (objectiveHealth < 0)
+            objectiveHealth = 0;
+        objectiveHealthBar.localScale = new Vector3(1f, objectiveHealth, 1f);
+    }
+
+    // detects attacks received
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "enemyWeapon")
+        {
+            // lose health
+            objectiveHealth -= damage;
+        }
+        // objective destroyed
+        if (objectiveHealth <= 0 && !playerController.gameIsOver)
+        {
+            playerController.gameIsOver = true;
+            playerController.gameOver();
+        }
+    }
+}
